68ef1e678771fae507b757c4720e7bb0f09cf81f
Bdotgo17 <bdotgo@hotmail.com>
2022-09-14 09:46:58 -0500

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Ralph";
+
+            int dogAge = 10;
+
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+
+        }
+    }
+}
+
